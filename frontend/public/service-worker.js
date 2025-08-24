ac0682884dbb22ccdeea1f949118a5c11582c7a6
seukaua1m <seukaua1m@gmail.com>
2025-08-24 13:23:36 -0300

    Update service-worker.js

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -10,22 +10,18 @@ const urlsToCache = [
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Install Event processing");
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("[Service Worker] Caching all: app shell and content");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log(`[Service Worker] Fetching resource: ${event.request.url}`);
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).then((response) => {
         return caches.open(CACHE_NAME).then((cache) => {
-          console.log(`[Service Worker] Caching new resource: ${event.request.url}`);
           cache.put(event.request, response.clone());
           return response;
         });
@@ -35,14 +31,12 @@ self.addEventListener("fetch", (event) => {
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activate Event processing");
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((keyList) =>
       Promise.all(
         keyList.map((key) => {
           if (!cacheWhitelist.includes(key)) {
-            console.log(`[Service Worker] Deleting cache: ${key}`);
             return caches.delete(key);
           }
         })
