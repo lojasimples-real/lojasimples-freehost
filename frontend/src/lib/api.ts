18622098556912541ac9e23c39ccb3c62c77ea54
seukaua1m <seukaua1m@gmail.com>
2025-08-24 11:47:47 -0300

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://app.lojasimples.site/api',
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     timeout: 5000, // Increase timeout
 });
 
